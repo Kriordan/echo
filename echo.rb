b6d6983b0b37d7aa3114cc8da887b046b337b584
kriordan <keith.riordan@gmail.com>
2013-11-24 23:09:59 -0500

    phase one of the challenge; added playback method which takes a single argument, poses question to user, gets input and repeats input back to user

--- echo.rb
+++ echo.rb
@@ -1,1 +1,11 @@
 # Beginning of the echo challenge
+
+def playback (text)
+  puts text.capitalize
+  input = gets.chomp
+  puts "You said: #{input}"
+end
+
+puts playback ("what do you want to say?")
+
+
