47a57f6e95f70931e552d8278122cc79a5f0d304
kriordan <keith.riordan@gmail.com>
2013-11-25 08:18:47 -0500

    added two methods, talkboy and yakbak, to handle an endlessinput of strings and a formatted return of the strings

--- echo.rb
+++ echo.rb
@@ -1,16 +1,36 @@
 # Beginning of the echo challenge
+@strings = []
 
-def playback (input)
-  if input == "Nothing!"
-    puts "Ok, fine!"
-  elsif input == "I have a lot to say"
-    puts "I don't have time for that right now!"
-  else
-    puts "You said: #{input}"
+def playback(convo)
+  puts "You said: #{convo}"
+end
+
+def talkboy(convo)# takes in strings until user enters "done"
+  puts "Ok, let's hear it!"
+  convo = gets.chomp
+  while convo != "done"
+    @strings << convo
+    convo = gets.chomp
   end
 end
 
-puts "what do you want to say?"
-playback (gets.chomp)
+def yakbak #returns formatted replay of input to the @strings array
+  puts "You said: #{@strings[0]}"
+  @strings[1, @strings.length-2].each do |snippet|
+    puts "Then, you said: #{snippet}"
+  end
+  puts "Finally you said: #{@strings[-1]}"
+  puts "Phew! Glad you got all #{@strings.length} of those things off your chest!"
+end
 
+puts "what do you want to say?"
+convo = gets.chomp
+if !(convo == "Nothing!" || convo == "I have a lot to say")
+  playback(convo)
+elsif convo == "Nothing!"
+  puts "Ok, fine!"
+elsif convo == "I have a lot to say"
+  talkboy(convo)
+  yakbak
+end
 
