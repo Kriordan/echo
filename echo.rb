c132fec0dec76ad28ddf4dd7f3bcdaceee8b3171
kriordan <keith.riordan@gmail.com>
2013-11-25 21:58:56 -0500

    Adding load strings from a file functionality:

--- echo.rb
+++ echo.rb
@@ -23,14 +23,27 @@ def yakbak #returns formatted replay of input to the @strings array
   puts "Phew! Glad you got all #{@strings.length} of those things off your chest!"
 end
 
+def gettysburg #requests filename for list of sayables
+  puts "Ok, where can I find what you want to say?"
+  notepad = gets.chomp
+  puts "Loading #{notepad}"
+  File.open(notepad, 'r').each_line do |line|
+    string = line
+    @strings << string
+  end
+end
+
 puts "what do you want to say?"
 convo = gets.chomp
-if !(convo == "Nothing!" || convo == "I have a lot to say")
+if !(convo == "Nothing!" || convo == "I have a lot to say" || convo == "I have something prepared")
   playback(convo)
 elsif convo == "Nothing!"
   puts "Ok, fine!"
 elsif convo == "I have a lot to say"
   talkboy(convo)
+  yakbak
+elsif convo == "I have something prepared"
+  gettysburg
   yakbak
 end
 
